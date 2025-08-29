f650cae2532949984d072d2b1289248a3676ea7d
AmedamaSpica <spica96amedama@gmail.com>
2025-08-29 18:02:34 +0900

    スクリプトの修正とprefabへの適用

--- src/Assets/Scripts/amedama script/ThroughBomb.cs
+++ src/Assets/Scripts/amedama script/ThroughBomb.cs
@@ -5,27 +5,25 @@ public class ThroughBomb : MonoBehaviour
 {
 
     [SerializeField] float throughTime = 0.2f;
+    [SerializeField] Collider BombCollider;
 
     WaitForSeconds _throughTime;//コライダーが機能するまでの時間
-    Collider BombCollider;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _throughTime = new WaitForSeconds(throughTime);
-        StartCoroutine("ColliderON");
 
-        if (TryGetComponent<Collider>(out Collider _BombCollider))
+        if (BombCollider != null)
         {
-            BombCollider = _BombCollider;
-            BombCollider.enabled = false;
+            StartCoroutine("ColliderON");
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        else
+        {
+            Debug.Log("Colliderがアタッチされていません。");
+            return;
+        }
     }
 
     IEnumerator ColliderON()//throughTime秒後にコライダーを機能させる処理
